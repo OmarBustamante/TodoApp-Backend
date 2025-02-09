38c6185becf919bd3221d930c0cab5d5f11ed5c1
OmarBustamante <omar.bustamante@encora.com>
2025-02-08 19:46:03 -0700

    Endpoint filter and page

--- src/main/java/com/encora/omar/bustamante/TodoApp/Backend/TodoController.java
+++ src/main/java/com/encora/omar/bustamante/TodoApp/Backend/TodoController.java
@@ -3,6 +3,7 @@ package com.encora.omar.bustamante.TodoApp.Backend;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -20,17 +21,17 @@ public class TodoController {
 
     public TodoController(){
         todos.add(new Todo(1, "Terminar backend", LocalDateTime.of(2025, 2, 10, 10, 0), false, null, Todo.Priority.HIGH, LocalDateTime.now()));
-        todos.add(new Todo(2, "Terminar Frontend", LocalDateTime.of(2025, 2, 12, 15, 0), false, null, Todo.Priority.HIGH, LocalDateTime.now()));
+        todos.add(new Todo(2, "Terminar Frontend", LocalDateTime.of(2025, 2, 12, 15, 0), true, null, Todo.Priority.HIGH, LocalDateTime.now()));
         todos.add(new Todo(3, "Terminar Main", LocalDateTime.of(2025, 2, 15, 7, 30), false, null, Todo.Priority.LOW, LocalDateTime.now()));
-        todos.add(new Todo(4, "Terminar backend", LocalDateTime.of(2025, 2, 10, 10, 0), false, null, Todo.Priority.LOW, LocalDateTime.now()));
+        todos.add(new Todo(4, "Terminar backend", LocalDateTime.of(2025, 2, 10, 10, 0), true, null, Todo.Priority.LOW, LocalDateTime.now()));
         todos.add(new Todo(5, "Terminar Frontend", LocalDateTime.of(2025, 2, 12, 15, 0), false, null, Todo.Priority.HIGH, LocalDateTime.now()));
         todos.add(new Todo(6, "Terminar Main", LocalDateTime.of(2025, 2, 15, 7, 30), false, null, Todo.Priority.LOW, LocalDateTime.now()));
-        todos.add(new Todo(7, "Terminar backend", LocalDateTime.of(2025, 2, 10, 10, 0), false, null, Todo.Priority.MEDIUM, LocalDateTime.now()));
-        todos.add(new Todo(8, "Terminar Frontend", LocalDateTime.of(2025, 2, 12, 15, 0), false, null, Todo.Priority.HIGH, LocalDateTime.now()));
+        todos.add(new Todo(7, "Terminar backend", LocalDateTime.of(2025, 2, 10, 10, 0), true, null, Todo.Priority.MEDIUM, LocalDateTime.now()));
+        todos.add(new Todo(8, "Terminar Frontend", LocalDateTime.of(2025, 2, 12, 15, 0), true, null, Todo.Priority.HIGH, LocalDateTime.now()));
         todos.add(new Todo(9, "Terminar Main", LocalDateTime.of(2025, 2, 15, 7, 30), false, null, Todo.Priority.HIGH, LocalDateTime.now()));
-        todos.add(new Todo(10, "Terminar backend", LocalDateTime.of(2025, 2, 10, 10, 0), false, null, Todo.Priority.MEDIUM, LocalDateTime.now()));
+        todos.add(new Todo(10, "Terminar backend", LocalDateTime.of(2025, 2, 10, 10, 0), true, null, Todo.Priority.MEDIUM, LocalDateTime.now()));
         todos.add(new Todo(11, "Terminar Frontend", LocalDateTime.of(2025, 2, 12, 15, 0), false, null, Todo.Priority.HIGH, LocalDateTime.now()));
-        todos.add(new Todo(12, "Terminar Main", LocalDateTime.of(2025, 2, 15, 7, 30), false, null, Todo.Priority.LOW, LocalDateTime.now()));
+        todos.add(new Todo(12, "Terminar Main", LocalDateTime.of(2025, 2, 15, 7, 30), true, null, Todo.Priority.LOW, LocalDateTime.now()));
     }
 
     @GetMapping("/")
@@ -43,28 +44,26 @@ public class TodoController {
         return todos;
     }
 
-    @GetMapping("/todos/{id}")
+    @GetMapping("/todos/todo/{id}")
     public Todo getTodoById(@PathVariable int id){
         Todo todo = todos.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
         if(todo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         return todo;
     }
 
-    @GetMapping("/todos/page/{num}")
-    public List<Todo> getPage(@PathVariable int num){
-        int end = num * 10;
-        int start = end - 10;
-        if(start >= todos.size()){
-            throw new ResponseStatusException(HttpStatus.NO_CONTENT);
-        } else if (end > todos.size()) {
-            end = todos.size();
-        }
-        return todos.subList(start, end);
-    }
+    @GetMapping("/todos/filter")
+    public List<Todo> getFilterList(
+            @RequestParam(required = false) String text,
+            @RequestParam(required = false) Todo.Priority priority,
+            @RequestParam(required = false) Boolean done,
+            @RequestParam(required = false) String sort
+    ){
+        List<Todo> page = todos;
+        page = page.stream().filter(todo -> text == null || todo.getText().toLowerCase().contains(text.toLowerCase()))
+                .filter(todo -> priority == null || todo.getPriority() == priority)
+                .filter(todo -> done == null || todo.isDone() == done)
+                .toList();
 
-    @GetMapping("/todos/page/{num}/{sort}")
-    public List<Todo> getSortedPage(@PathVariable int num,@PathVariable String sort){
-        List<Todo> page = getPage(num);
         switch(sort){
             case "high":
                 page = page.stream().sorted(Comparator.comparing(Todo::getPriority)).toList();
@@ -81,10 +80,32 @@ public class TodoController {
             case "dueLow":
                 page = page.stream().sorted(Comparator.comparing(Todo::getDueDate).thenComparing(Todo::getPriority, Comparator.reverseOrder())).toList();
                 break;
-            default:
+            case null, default:
                 break;
         }
-
         return page;
     }
+
+    @GetMapping("/todos/page/{num}")
+    public List<Todo> getPage(
+            @PathVariable int num,
+            @RequestParam(required = false) String text,
+            @RequestParam(required = false) Todo.Priority priority,
+            @RequestParam(required = false) Boolean done,
+            @RequestParam(required = false) String sort
+    ){
+        int end = num * 10;
+        int start = end - 10;
+
+        List<Todo> page = getFilterList(text, priority, done, sort);
+
+        if(start >= page.size()){
+            throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+        } else if (end > page.size()) {
+            end = page.size();
+        }
+
+        return page.subList(start, end);
+    }
+
 }
